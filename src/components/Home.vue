570918a6bb4ef4d3236385c6d81c8764b484df41
theji2bs <elhadad.malidi@hetic.net>
2017-04-16 20:50:20 +0200

    Carousel

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,26 @@
 <template>
-    <div class="home"></div>
+    <div class="home">
+        <swiper :options="swiperOption">
+            <swiper-slide v-for="work in works" :key="work.id">
+                <div class="carousel-work-container">
+                    <img class="carousel-work-cover" :src="work.cover">
+                    <div class="carousel-work-date">
+                        <p>{{work.date}}</p>
+                    </div>
+                    <div class="carousel-pagination">
+                        <span class="carousel-total">0{{works.length}}</span>
+                        <span class="pagination-separator"></span>
+                        <span class="carousel-current">0{{work.id}}</span>
+                    </div>
+                    <div class="carousel-work-title">
+                        <h2>{{work.title}}</h2>
+                        <h3>{{work.role}}</h3>
+                        <div class="title-separator"></div>
+                    </div>
+                </div>
+            </swiper-slide>
+        </swiper>
+    </div>
 </template>
 
 <script>
@@ -7,9 +28,20 @@
 
   export default{
     name: 'home',
+    data() {
+      return {
+        works: [],
+        swiperOption: {
+          mousewheelControl: true,
+          slidesPerView: 'auto',
+          centeredSlides: true,
+          spaceBetween: 110,
+        },
+      };
+    },
     mounted() {
       works.query().then((response) => {
-        console.log(response.data);
+        this.works = response.data;
       });
     },
   };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue';
+import VueAwesomeSwiper from 'vue-awesome-swiper';
+
 import App from './App';
 import router from './router';
+
+// Libs used by Vue
+Vue.use(VueAwesomeSwiper);
 
 Vue.config.productionTip = false;
 
