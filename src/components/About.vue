20041ef935148203d99150d4f1bb520a375ea087
theji2bs <elhadad.malidi@hetic.net>
2017-04-17 17:17:29 +0200

    About

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,33 @@
 <template>
     <div class="about">
-        <h1>About</h1>
+        <div class="about-container">
+            <h1>Elhadad Malidi</h1>
+            <h2>Front End Developer</h2>
+            <div class="about-description">
+                <h3>About me</h3>
+                <p>
+                    Yo, my name is <span class="name">Elhadad Malidi</span>. I’m a web developer based in Paris. I’m currently a student at <a class="hetic" href="https://www.hetic.net/" target="_blank">HETIC</a>, first webschool in France.<br>
+                    I’m looking for an internship for 6 months between July and December 2017 as an web developer.<br>
+                    You can download my resume <a class="resume" href="#">here</a>. Feel free to contact me...
+                </p>
+            </div>
+            <div class="about-socials">
+                <ul>
+                    <li>
+                        <a href="https://twitter.com/MalidiElhadad" target="_blank"><p>Twitter</p></a>
+                    </li>
+                    <li>
+                        <a href="https://www.linkedin.com/in/elhadad-malidi-ab1287aa/" target="_blank"><p>Linkedin</p></a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/theji2bs" target="_blank"><p>Github</p></a>
+                    </li>
+                    <li>
+                        <a href="mailto:elhadad.malidi@hetic.net"><p>Email</p></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
 </template>
 
